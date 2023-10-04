100a9cf961e530d85717511a85a641a92a40c23c
Lorenzo Gonçalves <143613079+LorenzQG@users.noreply.github.com>
2023-10-04 17:38:58 -0300

    Atualizando Multiplo.js

--- LogProg/aula9/Multiplo.js
+++ LogProg/aula9/Multiplo.js
@@ -2,10 +2,14 @@ const prompt = require("prompt-sync")()
 
 const num = Number(prompt("Digite o numero: "))
 
+if(num  >= 5){
 for(let i = 1; i <= num; i++){
     let verifica = i % 5;
     if(verifica == 0){
         console.log(`${i}`)
     }
     
+}
+}else{
+    console.log("Numero tem que ser 5 ou maior")
 }
